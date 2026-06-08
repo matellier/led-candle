48dee88c9f8cc0a8271428af30a57197a24f2727
Mark Tellier <mark.tellier@gmail.com>
2026-06-08 15:15:11 -0400

    misc

---
+++ firmware/arduino/candle_flicker/candle_flicker1/candle_flicker1.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+Adafruit_NeoPixel strip(1, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+}
+
+void loop() {
+
+  int r = 200 + random(55);
+  int g = 90 + random(40);
+  int b = random(25);
+
+  strip.setPixelColor(0, r, g, b);
+  strip.show();
+
+  delay(random(30,120));
+}
